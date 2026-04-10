59232c4c6b4fccd60c6bc8c52306758e5586cf94
Victor <victor.souza.pro@outlook.com>
2026-04-10 10:52:34 -0300

    fake logout function added to logout button component

--- components/layout/AppHeader.tsx
+++ components/layout/AppHeader.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { SidebarTrigger } from "../ui/sidebar"
-import { User } from "lucide-react"
+import { User} from "lucide-react"
 import { usePathname } from "next/navigation"
 import { LogoutBtn } from "../ui/logout-button"
 
--- components/ui/logout-button.tsx
+++ components/ui/logout-button.tsx
@@ -1,13 +1,23 @@
 import { Button } from "./button"
 import { LogOut } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 
 export const LogoutBtn = () => {
+
+    const router = useRouter()
+    
+    async function handleLogout() {
+    
+    router.push("/login")
+  }
+
   return (
     <Button 
     size="sm" 
     variant="ghost" 
     className="cursor-pointer"
+    onClick={handleLogout}
     >
         <LogOut/>
     </Button>
